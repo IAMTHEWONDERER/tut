35de0f4ff95987c316abb924f45b89363786caab
Oussama Alouche <theoblivionitself@gmail.com>
2024-04-04 01:42:33 +0000

    added libraries

---
+++ automation.py
@@ -0,0 +1,12 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from email.utils import formataddr
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+PORT = 587
+EMAIL_SERVER = "smtp.gmail.com"
+
+#loading everything
